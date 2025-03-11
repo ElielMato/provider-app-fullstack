c11a820c430d3486663a5996aecf12ca30ce750b
Eliel Mato <86053896+ElielMato@users.noreply.github.com>
2025-03-11 09:34:51 -0300

    Update database.py

--- backend/database.py
+++ backend/database.py
@@ -2,8 +2,8 @@ from flask_sqlalchemy import SQLAlchemy # type: ignore
 
 db = SQLAlchemy()
 
-USER_DB='eliel'
-PASS_DB='200403'
+USER_DB='root'
+PASS_DB=''
 URL_DB='localhost'
-NAME_DB='proveedorapp'
+NAME_DB='proveedor'
 FULL_URL_DB=f'postgresql://{USER_DB}:{PASS_DB}@{URL_DB}/{NAME_DB}'
